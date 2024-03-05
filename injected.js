650b3cfa2edb1055a278625d6a5754203a0cc394
Hadrian Finch <hadrianfinch@outlook.com>
2024-03-04 21:48:01 -0800

    pre refactor

--- injected.js
+++ injected.js
@@ -1,8 +1,10 @@
-var foo = 10;
 
-// document.head.appendChild(
+// document.querySelector("#taught-courses-switcher").appendChild(
 //     (() => {
-//         const elm = document.createElement("div");
+//         const elm = document.createElement("i");
+
+//         // elm.classList.add("fa-solid");
+//         // elm.classList.add("fa-calendar-days");
 
 //         // elm.src = "https://kit.fontawesome.com/09a93a2370.js";
 //         // elm.crossOrigin = "anonymous";
@@ -11,3 +13,29 @@ var foo = 10;
 //         return elm;
 //     })()
 // );
+
+const importantPost = document.querySelector("#important-post");
+// const pageHeadder = document.querySelector("#center-top");
+if (importantPost != null)
+{
+    const mainInner = document.querySelector("#main-inner");
+    mainInner.insertBefore(importantPost, mainInner.firstChild);
+}
+
+
+// const classAdditions = {
+//     sr_button: [
+//         ".sgy-tabbed-navigation > li"
+//     ]
+// };
+
+// for (const key in classAdditions) {
+//     const element = classAdditions[key];
+
+//     element.forEach((selector) => {
+//         const objs = document.querySelectorAll(selector);
+//         objs.forEach((obj) => {
+//             obj.classList.add(key);
+//         });
+//     });
+// }
